942ade05c10e408eb995bf78ebcf6136854a3f49
Shaeel786 <shaeel678@gmail.com>
2023-08-02 17:13:21 +0500

    Spec file added

--- src/modules/orders/orders.controller.spec.ts
+++ src/modules/orders/orders.controller.spec.ts
@@ -1,18 +1,55 @@
-import { Test, TestingModule } from '@nestjs/testing';
+// orders.controller.spec.ts
+
+import { Test } from '@nestjs/testing';
 import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { Order } from '../../models/order.model';
 
 describe('OrdersController', () => {
-  let controller: OrdersController;
+  let ordersController: OrdersController;
+  let ordersService: OrdersService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [OrdersController],
+      providers: [OrdersService],
     }).compile();
 
-    controller = module.get<OrdersController>(OrdersController);
+    ordersService = moduleRef.get<OrdersService>(OrdersService);
+    ordersController = moduleRef.get<OrdersController>(OrdersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getAllOrders', () => {
+    it('should return an array of orders', async () => {
+      const mockOrders: any[] = [
+        { id: 1, side: 'buy', price: 10 },
+        { id: 2, side: 'sell', price: 20 },
+      ];
+
+      jest.spyOn(ordersService, 'findAllOrders').mockResolvedValue(mockOrders);
+
+      const result = await ordersController.getAllOrders();
+      expect(result).toEqual(mockOrders);
+    });
+  });
+
+  describe('createOrder', () => {
+    it('should create a new order', async () => {
+      const newOrder: Partial<Order> = {
+        side: 'buy',
+        price: 15,
+      };
+
+      const mockCreatedOrder: any = {
+        id: 3,
+        side: 'buy',
+        price: 15,
+      };
+
+      jest.spyOn(ordersService, 'createOrder').mockResolvedValue(mockCreatedOrder);
+
+      const result = await ordersController.createOrder(newOrder);
+      expect(result).toEqual(mockCreatedOrder);
+    });
   });
 });
